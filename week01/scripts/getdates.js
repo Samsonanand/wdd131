f5892b356089fb3e002c7c6b761ab71e6df1afa5
Samsonanand <samsonanand.k@gmail.com>
2025-01-15 16:58:28 +0530

    changes in java script

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,2 +1,5 @@
-
-document.getElementById("lastModified").textContent = document.lastModified;
+// Update the "Last Modified" date in the footer
+const lastModifiedElement = document.getElementById("lastModified");
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = document.lastModified;
+}
