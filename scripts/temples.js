7703001b8f089f45c58255f6908f5ca467df48b0
Samsonanand <samsonanand.k@gmail.com>
2025-01-19 01:50:52 +0530

    edited js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,14 +1,1 @@
-// Update the "Last Modified" date in the footer
-// minified version
-document.getElementById("lastModified")&&(document.getElementById("lastModified").textContent=document.lastModified);
-
-
-document.getElementById("menu-toggle").addEventListener("click",function(){document.getElementById("navbar").classList.toggle("active")});
-
-// Dynamically load a non-essential script
-if (window.innerWidth < 600) {
-    const script = document.createElement('script');
-    script.src = 'non-essential-script.js';
-    script.async = true;
-    document.body.appendChild(script);
-}
+document.getElementById("lastModified")?.textContent=document.lastModified;document.getElementById("menu-toggle").addEventListener("click",()=>document.getElementById("navbar").classList.toggle("active"));if(window.innerWidth<600){const e=document.createElement("script");e.src="non-essential-script.js",e.async=!0,document.body.appendChild(e);}
