4360fb6c413bb01c83b26b8e652d34f598a0b6f1
Samsonanand <samsonanand.k@gmail.com>
2025-01-19 01:31:30 +0530

    reverted back

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,5 @@
-// JavaScript for toggling the mobile menu
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('active'); // Toggle active class to show/hide the menu
-});
+// Update the "Last Modified" date in the footer
+const lastModifiedElement = document.getElementById("lastModified");
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = document.lastModified;
+}
