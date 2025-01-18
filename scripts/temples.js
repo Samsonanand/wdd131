05414c99808a9d8a95c000475c6d0b8d832a1b83
Samsonanand <samsonanand.k@gmail.com>
2025-01-19 01:42:06 +0530

    edited to get better assesibiility

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,14 @@
 // Update the "Last Modified" date in the footer
-const lastModifiedElement = document.getElementById("lastModified");
-if (lastModifiedElement) {
-    lastModifiedElement.textContent = document.lastModified;
+// minified version
+document.getElementById("lastModified")&&(document.getElementById("lastModified").textContent=document.lastModified);
+
+
+document.getElementById("menu-toggle").addEventListener("click",function(){document.getElementById("navbar").classList.toggle("active")});
+
+// Dynamically load a non-essential script
+if (window.innerWidth < 600) {
+    const script = document.createElement('script');
+    script.src = 'non-essential-script.js';
+    script.async = true;
+    document.body.appendChild(script);
 }
