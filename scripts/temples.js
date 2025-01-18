9ff46c7af14c4b39ea644e872aa7d62208c99353
Samsonanand <samsonanand.k@gmail.com>
2025-01-19 00:52:33 +0530

    temples album

---
+++ scripts/temples.js
@@ -0,0 +1,5 @@
+// Update the "Last Modified" date in the footer
+const lastModifiedElement = document.getElementById("lastModified");
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = document.lastModified;
+}
